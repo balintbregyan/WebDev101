7ac0a80256e844e27b290f2f236b0fd7d21bce04
balintbregyan <balint.bregyan@gmail.com>
2025-11-04 08:24:41 +0100

    1st Challenge

--- JS/01-Fundamentals-Part-1/starter/script.js
+++ JS/01-Fundamentals-Part-1/starter/script.js
@@ -1,3 +1,4 @@
+/*
 let js = "amazing";
 console.log(40 + 8 + 23 - 10);
 
@@ -5,8 +6,44 @@ console.log("Jonas");
 console.log(23);
 
 let firstName = "Matilda";
+let first = "Jonas";
+let firstNamePerson = "Bob";
+let first_name = "Alice";
 
 console.log(firstName);
 console.log(firstName);
 console.log(firstName);
+
+// Variable name conventions
+let jonas_matilda = "JM";
+let $function = 27;
+
+let person = "jonas";
 let PI = 3.1415;
+
+let myFirstJob = "Coder";
+let myCurrentJob = "Teacher";
+
+let job1 = "programmer";
+let job2 = "teacher";
+
+console.log(myFirstJob);*/
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof "Jonas");
+
+javascriptIsFUn = "YES";
+console.log(typeof javascriptIsFUn);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+console.log(typeof null);
