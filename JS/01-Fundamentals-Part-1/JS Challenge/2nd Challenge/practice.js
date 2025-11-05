36b8d7a704475cf8c8fbf4cec43b28e12ca49494
balintbregyan <balint.bregyan@gmail.com>
2025-11-05 10:22:39 +0100

    2nd Challenge Tutor's Solution

--- JS/01-Fundamentals-Part-1/JS Challenge/2nd Challenge/practice.js
+++ JS/01-Fundamentals-Part-1/JS Challenge/2nd Challenge/practice.js
@@ -1,7 +1,31 @@
+// 2nd Challenge My Solution
+
+// const massMark = 78;
+// const heightMark = 1.69;
+// const massJohn = 92;
+// const heightJohn = 1.95;
+
+// const BMIMark = massMark / (heightMark * heightMark);
+// const BMIJohn = massJohn / (heightJohn * heightJohn);
+// console.log(BMIMark, BMIJohn);
+
+// if (BMIMark > BMIJohn) {
+//   console.log(`Mark's BMI (${BMIMark}) is higher than John's (${BMIJohn})!`);
+// } else {
+//   console.log(`John's BMI (${BMIJohn}) is higher than Mark's (${BMIMark})!`);
+// }
+
+//  2nd Challenge Tutor's Solution
+
 const massMark = 78;
 const heightMark = 1.69;
 const massJohn = 92;
 const heightJohn = 1.95;
+
+// const massMark = 95;
+// const heightMark = 1.88;
+// const massJohn = 85;
+// const heightJohn = 1.76;
 
 const BMIMark = massMark / (heightMark * heightMark);
 const BMIJohn = massJohn / (heightJohn * heightJohn);
