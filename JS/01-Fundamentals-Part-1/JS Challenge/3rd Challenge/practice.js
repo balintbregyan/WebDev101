e3336701a6ff2594cb57e75fa7972319d1c65524
balintbregyan <balint.bregyan@gmail.com>
2025-11-11 09:01:42 +0100

    3rd Challenge Tutor's Solution

--- JS/01-Fundamentals-Part-1/JS Challenge/3rd Challenge/practice.js
+++ JS/01-Fundamentals-Part-1/JS Challenge/3rd Challenge/practice.js
@@ -1,52 +1,32 @@
-// // Dolphins's average score
-// const dolphinScore1 = 96;
-// const dolphinScore2 = 108;
-// const dolphinScore3 = 89;
+// const scoreDolphins = (96 + 108 + 89) / 3;
+// const scoreKoalas = (88 + 91 + 110) / 3;
 
-// // Koalas's average score
-// const koalaScore1 = 88;
-// const koalaScore2 = 91;
-// const koalaScore3 = 110;
+// console.log(scoreDolphins, scoreKoalas);
 
-// Dolphins's average score
-const dolphinScore1 = 97;
-const dolphinScore2 = 112;
-const dolphinScore3 = 101;
-
-// Koalas's average score
-const koalaScore1 = 109;
-const koalaScore2 = 95;
-const koalaScore3 = 110;
-
-// Calculate average scores
-const dolphinAverage = (dolphinScore1 + dolphinScore2 + dolphinScore3) / 3;
-const koalaAverage = (koalaScore1 + koalaScore2 + koalaScore3) / 3;
-
-// Minimum score to win
-const minScore = 100;
-
-console.log("Dolphins Average Score:", dolphinAverage);
-console.log("Koalas Average Score:", koalaAverage);
-
-// Main task
-// if (dolphinAverage > koalaAverage) {
-//   console.log("Dolphins wins the trophy! 🏆");
-// } else if (koalaAverage > dolphinAverage) {
-//   console.log("Koalas wins the trophy! 🏆");
-// } else if (dolphinAverage === koalaAverage) {
-//   console.log("This is a draw!");
+// if (scoreDolphins > scoreKoalas) {
+//   console.log("Dolphins win the trophy 🏆");
+// } else if (scoreKoalas > scoreDolphins) {
+//   console.log("Koalas win the trophy 🏆");
+// } else {
+//   console.log("It's a draw!");
 // }
 
-// Bonus 1-2
-if (dolphinAverage > koalaAverage && dolphinAverage > minScore) {
-  console.log("Dolphins wins the trophy! 🏆");
-} else if (koalaAverage > dolphinAverage && koalaAverage > minScore) {
-  console.log("Koalas wins the trophy! 🏆");
-} else if (dolphinAverage === koalaAverage) {
-  console.log("This is a draw!");
+// Bonus 1
+
+const scoreDolphins = (97 + 112 + 80) / 3;
+const scoreKoalas = (109 + 95 + 50) / 3;
+console.log(scoreDolphins, scoreKoalas);
+
+if (scoreDolphins > scoreKoalas && scoreDolphins >= 100) {
+  console.log("Dolphins win the trophy 🏆");
+} else if (scoreKoalas > scoreDolphins && scoreKoalas >= 100) {
+  console.log("Koalas win the trophy 🏆");
 } else if (
-  (dolphinAverage === koalaAverage && !dolphinAverage < minScore) ||
-  !koalaAverage < minScore
+  scoreDolphins === scoreKoalas &&
+  scoreDolphins >= 100 &&
+  scoreKoalas >= 100
 ) {
-  console.log("None of the teams reached the minimum Score!");
+  console.log("Both win the trophy 🏆");
+} else {
+  console.log("No team wins the trophy 😥");
 }
