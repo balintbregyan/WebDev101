c2868c150c26260b65dc4dde30cdeabb3145ec8d
balintbregyan <balint.bregyan@gmail.com>
2025-11-11 10:42:06 +0100

    Switch Statement

--- JS/01-Fundamentals-Part-1/starter/script.js
+++ JS/01-Fundamentals-Part-1/starter/script.js
@@ -205,7 +205,7 @@ if (favourite === 23) {
   console.log("Number is not 23 or 7 or 9");
 }
 
-if (favourite !== 23) console.log("Why not 23?");*/
+if (favourite !== 23) console.log("Why not 23?");
 
 const hasDriverLicense = true; // A
 const hasGoodVision = true; // B
@@ -229,4 +229,46 @@ if (hasDriverLicense && hasGoodVision && !isTired) {
   console.log("Sarah is able to drive!");
 } else {
   console.log("Someone else should drive...");
+}*/
+
+const day = "thursday";
+
+// Switch Statement
+switch (day) {
+  case "monday": // day === 'monday'
+    console.log("Plan course structure");
+    console.log("Go to coding meetup");
+  // break;
+  case "tuesday":
+    console.log("Prepare theory videos");
+    break;
+  case "wednesday":
+  case "thursday":
+    console.log("Write code examples");
+    break;
+  case "friday":
+    console.log("Record videos");
+    break;
+  case "saturday":
+  case "sunday":
+    console.log("Enjoy the weekend :D");
+    break;
+  default:
+    console.log("Not a valid day!");
+}
+
+// Same statement with IF/ELSE Logic
+if (day === "monday") {
+  console.log("Plan course structure");
+  console.log("Go to coding meetup");
+} else if (day === "tuesday") {
+  console.log("Prepare theory videos");
+} else if (day === "wednesday" || day === "thursday") {
+  console.log("Write code examples");
+} else if (day === "friday") {
+  console.log("Record videos");
+} else if (day === "saturday" || day === "sunday") {
+  console.log("Enjoy the weekend :D");
+} else {
+  console.log("Not a valid day!");
 }
